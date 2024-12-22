131dcf01e7c44d0d58fa2d40ac0ea84752b214a8
Alexandr-U8 <utkin88nya@yandex.ru>
2024-12-22 22:46:12 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,15 @@
+# TestSuit
+# импорт необходимых библиотек и файлов
+import unittest
+import test_12_3
+
+# объявление переменной suite_test объекта TestSuite модуля unittest
+suite_test = unittest.TestSuite()
+# Добавляю тесты RunnerTest и TournamentTest в этот TestSuit.
+suite_test.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.RunnerTest))
+suite_test.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.TournamentTest))
+
+# Создание объекта класса TextTestRunner, с аргументом verbosity=2.
+runner = unittest.TextTestRunner(verbosity=2)
+# запуск объекта runner
+runner.run(suite_test)
